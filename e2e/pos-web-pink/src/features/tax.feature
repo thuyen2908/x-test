990bedde861a3d6eb0afed3cc517dbcd019d7911
Thylia <97781772+thuyen2908@users.noreply.github.com>
2025-10-05 21:52:34 -0400

    build(pos-web-pink): test scripts init
    * build(pos-web-pink): setup pos pink with initial test cases
    
    * feat(pos-web-pink): create ticket for waiting
    
    ---------
    
    Co-authored-by: Thuyen Nguyen <thuyen.nguyen@xsofts.vn>

---
+++ e2e/pos-web-pink/src/features/tax.feature
@@ -0,0 +1,10 @@
+@regression @smoke
+Feature: Tax management
+
+  Scenario: User can view the list of tax
+    Given I am on the HOME page
+    When I click on the header menu
+    And I select the "Back Office" label in the menu list
+    And I select the "Tax" label in the expanded list
+    Then I should be redirected to TAX page
+    And I should see the "Tax" screen
---
+++ e2e/pos-web-pink/src/test-config.ts
@@ -0,0 +1,31 @@
+import type { Browser } from '#types';
+
+import { env } from './env';
+
+/**
+ * Test configuration
+ */
+export interface TestConfig {
+	adminName: string;
+	adminEmail: string;
+	adminPassword: string;
+
+	timezone: string;
+	merchantName: string;
+}
+
+/**
+ * Test configuration provider function
+ */
+export const getConfig = (browser: Browser = 'chrome'): TestConfig => {
+	const { posConfig } = env;
+
+	return {
+		adminName: posConfig.adminName,
+		adminEmail: posConfig.adminEmail,
+		adminPassword: posConfig.adminPassword,
+
+		timezone: posConfig[browser].timezone,
+		merchantName: posConfig[browser].merchantName,
+	};
+};
